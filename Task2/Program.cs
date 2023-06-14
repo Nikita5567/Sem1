f24751100e55abf63acbff3b6c271046ce65157e
Nikita5567 <ilin10988@yandex.ru>
2023-06-14 20:22:32 +0300

    Сделал домашку к первому семинару

---
+++ Task2/Program.cs
@@ -0,0 +1,17 @@
+﻿//  Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.WriteLine("Введите первое число");
+string numberA = Console.ReadLine()!;
+ int numberIntA = Convert.ToInt32(numberA);
+
+
+Console.WriteLine("Введите второе число");
+string numberB = Console.ReadLine()!;
+ int numberIntB = Convert.ToInt32(numberB);
+ if (numberIntA < numberIntB)
+ {
+    Console.WriteLine("max = numberB");
+ }
---
+++ Task4/Program.cs
@@ -0,0 +1,38 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.WriteLine("Введите первое число");
+ string numberA = Console.ReadLine()!;
+ int numberIntA = Convert.ToInt32(numberA);
+ 
+Console.WriteLine("Введите второе число");
+ string numberB = Console.ReadLine()!;
+ int numberIntB = Convert.ToInt32(numberB);
+ 
+Console.WriteLine("Введите третье число");
+ string numberC = Console.ReadLine()!;
+ int numberIntC = Convert.ToInt32(numberC);
+ int max = 0;
+
+ 
+ if (numberIntA > numberIntB){
+   max = numberIntA;
+ } else {
+   max = numberIntB;
+ }
+ if (numberIntC > max){
+   max = numberIntC;
+ }
+  Console.WriteLine("Наибольшее из 3-х чисел " + max);
+   
+ 
+
+
+
+
+
+   
+
+ 
